39638295064f414b92099671a0da724467269319
Nicolas Wallace A. P. <59179465+lispector-asm@users.noreply.github.com>
2025-05-26 08:17:45 -0300

    CORS em main.py

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from app import database, routes, models
+from fastapi.middleware.cors import CORSMiddleware
 
 models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI()
 app.include_router(routes.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
